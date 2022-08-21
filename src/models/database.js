7c21f89cd9db66846682d1cf483bd4bf360cbe0e
Mohammad Ardi Trisnaldi <53711786+Aldyy123@users.noreply.github.com>
2022-08-21 10:08:40 +0700

    Update database.js

--- src/models/database.js
+++ src/models/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://arsy:aldyy@cluster1.rcp14.mongodb.net/ArsyDigicom?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://arsy:tryingdatabase@cluster1.rcp14.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).catch(e => console.log(e))
